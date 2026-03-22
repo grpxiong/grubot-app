7bf48effce7da9697bb7768288dba0208ed4e942
grpxiong <145522822+grpxiong@users.noreply.github.com>
2026-03-22 10:21:24 -0500

    Update Supabase URL and key in config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.ENV = {
-  SUPABASE_URL: "https://your-project.supabase.co",
-  SUPABASE_KEY: "your-public-anon-key"
+  SUPABASE_URL: "https://fzrpjbojyzzohcypgfzm.supabase.co",
+  SUPABASE_KEY: "sb_publishable_6WvoLA-b4diZ4_omsc-XUg_5iNrppdz",
+  N8N_WEBHOOK_URL: "https://hejan.app.n8n.cloud/webhook/1ada8f2d-1ba6-4b0b-b755-caf712fbb699"
 };
