e7737762678a8d9ddae6512ea83d9d9d20cb231a
grpxiong <145522822+grpxiong@users.noreply.github.com>
2026-03-30 18:39:15 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.ENV = {
   SUPABASE_URL: "https://fzrpjbojyzzohcypgfzm.supabase.co",
   SUPABASE_KEY: "sb_publishable_6WvoLA-b4diZ4_omsc-XUg_5iNrppdz",
-  N8N_WEBHOOK_URL: "https://hejan.app.n8n.cloud/webhook/1ada8f2d-1ba6-4b0b-b755-caf712fbb699",
+  N8N_WEBHOOK_URL: "http://216.238.104.205:5678/webhook/1ada8f2d-1ba6-4b0b-b755-caf712fbb699",
   EDGE_FUNCTION_URL: "https://fzrpjbojyzzohcypgfzm.supabase.co/functions/v1/dynamic-action"
 };
