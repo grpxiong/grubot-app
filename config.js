8e35eb2081f6530d14792ee65bca62afb5ce4d5d
grpxiong <145522822+grpxiong@users.noreply.github.com>
2026-04-01 16:29:42 -0500

    Add GOOGLE_MAPS_KEY to environment configuration

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ window.ENV = {
   SUPABASE_URL: "https://fzrpjbojyzzohcypgfzm.supabase.co",
   SUPABASE_KEY: "sb_publishable_6WvoLA-b4diZ4_omsc-XUg_5iNrppdz",
   N8N_WEBHOOK_URL: "http://216.238.104.205:5678/webhook/1ada8f2d-1ba6-4b0b-b755-caf712fbb699",
-  EDGE_FUNCTION_URL: "https://fzrpjbojyzzohcypgfzm.supabase.co/functions/v1/dynamic-action"
+  EDGE_FUNCTION_URL: "https://fzrpjbojyzzohcypgfzm.supabase.co/functions/v1/dynamic-action",
+  GOOGLE_MAPS_KEY: "AIzaSyBDct-lj6DlHAlHkreQUcvoYdaGRCc3zlI",
 };
