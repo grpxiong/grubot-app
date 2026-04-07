4c011de373874389d6dee24946727c1891981193
grpxiong <145522822+grpxiong@users.noreply.github.com>
2026-04-06 22:52:51 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 window.ENV = {
   SUPABASE_URL: "https://fzrpjbojyzzohcypgfzm.supabase.co",
   SUPABASE_KEY: "sb_publishable_6WvoLA-b4diZ4_omsc-XUg_5iNrppdz",
-  N8N_WEBHOOK_URL: "https://grubot.duckdns.org/webhook/1ada8f2d-1ba6-4b0b-b755-caf712fbb699",
   EDGE_FUNCTION_URL: "https://fzrpjbojyzzohcypgfzm.supabase.co/functions/v1/dynamic-action",
   GOOGLE_MAPS_KEY: "AIzaSyBOOXhGw1KUcWPCc_DeQ8b-TfpnWc9wSGw",
 };
